04854d2312de3b030d8787aada06deecd85becb4
lexraijmakers <lex.raijmakers@gmail.com>
2023-05-26 13:53:53 +0200

    fix: configure port

--- src/server.ts
+++ src/server.ts
@@ -5,10 +5,11 @@ import { schema } from './schema'
 
 const start = async () => {
     const server = new ApolloServer<Context>({ schema })
+    const port = Number.parseInt(process?.env?.PORT || '4000') || 4000
 
     const { url } = await startStandaloneServer(server, {
         context: createContext,
-        listen: { port: 4000 }
+        listen: { port }
     })
 
     console.log(`🚀 Server ready at: ${url}`)
